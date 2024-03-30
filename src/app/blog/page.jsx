649ba87e4d36149d202cd92c674364cee8abeff6
Ashish <ashish.satpathy@gmail.com>
2024-03-30 16:40:09 +0530

    reload

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import { getPosts } from "@/lib/data";
 //FETCH DATA WITH API
 
 const getData = async () => {
-  const res = await fetch('https://quod-invicta.vercel.app/api/blog', {cache: "default"});
+  const res = await fetch('https://quod-invicta.vercel.app/api/blog', {cache: "reload"});
   if (!res.ok) {
     throw new Error("Something went wrong");
   }
