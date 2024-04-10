5072b6e46c32a4d86de7ac34a86738811cf8d3ef
Ashish <ashish.satpathy2003@gmail.com>
2024-04-11 02:31:28 +0530

    changed git settings

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./footer.module.css";
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"></link>
 const Footer = () => {
   return (
     <div className={styles.container}>
