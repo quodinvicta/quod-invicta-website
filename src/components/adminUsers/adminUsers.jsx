093c71af09ba24e4aa91831f119444cc93590103
Ashish <ashish.satpathy2003@gmail.com>
2024-05-25 14:42:30 +0530

    noavatar fix

--- src/components/adminUsers/adminUsers.jsx
+++ src/components/adminUsers/adminUsers.jsx
@@ -9,11 +9,13 @@ const AdminUsers = async () => {
   return (
     <div className={styles.container}>
       <h1>Users</h1>
+      
+    
       {users.map((user) => (
         <div className={styles.user} key={user.id}>
           <div className={styles.detail}>
             <Image
-              src={user.img || "/noAvatar.png"}
+              src={user.img || "/noavatar.png"}
               alt=""
               width={50}
               height={50}
@@ -26,6 +28,7 @@ const AdminUsers = async () => {
           </form>
         </div>
       ))}
+      
     </div>
   );
 };
