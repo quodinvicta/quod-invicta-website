e9cbc005be1ca0cc7dcb6b0445ca60134a662210
srijan <srijanswapnil246@gmail.com>
2025-07-20 23:30:58 +0530

    bg-black

--- app/blog/page.js
+++ app/blog/page.js
@@ -321,7 +321,7 @@ export default function FashionBlogPage() {
   );
 
   return (
-    <div className="min-h-screen ">
+    <div className="min-h-screen bg-black">
       <Navbar />
 
       <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-22">
